be8ebd9e23b7f55ffe7cd66fa74bdef9b22806a7
Uwe Mueller <uwe.mueller@apeisia.com>
2022-09-13 09:18:00 +0200

    types

---
+++ index.d.ts
@@ -0,0 +1,5 @@
+export var Signature: typeof import("./types/index").Signature;
+export var PublicKey: typeof import("./types/index").PublicKey;
+export var PrivateKey: typeof import("./types/index").PrivateKey;
+export var Ecdsa: typeof import("./types/index");
+export var utils: typeof import("./types/index");
