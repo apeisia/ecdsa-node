caf9e57e0f7b648ecda4ac9af03bbe4d5a6008b0
Philipp Burmeister <philipp.burmeister@apeisia.com>
2022-09-13 10:31:15 +0200

    ts types

--- index.d.ts
+++
@@ -1,5 +0,0 @@
-export var Signature: typeof import("./types/index").Signature;
-export var PublicKey: typeof import("./types/index").PublicKey;
-export var PrivateKey: typeof import("./types/index").PrivateKey;
-export var Ecdsa: typeof import("./types/index");
-export var utils: typeof import("./types/index");
---
+++ starbank-ecdsa.d.ts
@@ -0,0 +1,7 @@
+declare module "starbank-ecdsa" {
+  export var Signature: typeof import("./types/ellipticcurve/signature").Signature;
+  export var PublicKey: typeof import("./types/ellipticcurve/publicKey").PublicKey;
+  export var PrivateKey: typeof import("./types/ellipticcurve/privateKey").PrivateKey;
+  export var Ecdsa: typeof import("./types/ellipticcurve/ecdsa");
+  export var utils: typeof import("./types/ellipticcurve/utils/utils");
+}
